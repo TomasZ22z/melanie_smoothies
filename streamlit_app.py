13ef1c94b4e141e5bf7a4c1ee4588196bf5abab9
TomasZ22z <ju_jutsu@centrum.sk>
2024-09-27 16:08:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ session = cnx.session()
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json()
